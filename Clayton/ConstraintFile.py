9c262a9c2afc068ddfbd53d96fea0756a35ce926
Hailey <haileystrobelt@gmail.com>
2022-01-25 18:48:28 -0700

    Rig Work

---
+++ Clayton/ConstraintFile.py
@@ -0,0 +1,36 @@
+import maya.cmds as cmds
+
+
+def locator():
+    
+    sels = cmds.ls(sl=True)
+
+    bbox = cmds.xform(sels, q= True, boundingBox=True, ws=True) #[xmin, ymin, zmin, xmax, ymax, zmax]
+
+    mid_x = (bbox[0] + bbox[3]) / 2
+    mid_y = (bbox[1] + bbox[4]) / 2
+    mid_z = (bbox[2] + bbox[5]) / 2
+
+    loc = cmds.spaceLocator(position=[0,0,0], absolute=True)[0]
+
+    cmds.xform(loc, translation=[mid_x, mid_y, mid_z], ws=True)
+    
+
+
+def joints_from_sels():
+    sels = cmds.ls(sl=True)
+    
+    for sel in sels:
+        pos = cmds.xform(sel, q=True, rotatePivot=True, ws=True) #[x,y,z]
+        
+        cmds.select(cl=True)
+        n_joint = cmds.joint(position=[0,0,0], absolute=True)
+        cmds.xform(n_joint, translation=pos, ws=True)
+
+def parent_from_sels():
+    sels = cmds.ls(sl=True)
+    
+    for i, sel in enumerate(sels):
+        if i < (len(sels) - 1):
+            cmds.parent(sels[i], sels[i+1])
+        
---
+++ Clayton/myTools.py
@@ -0,0 +1,14 @@
+import maya.cmds as cmds
+
+def display_joint_orient():
+    sels = cmds.ls(sl=True)
+    
+    for sel in sels:
+        if cmds.nodeType(sel) == 'joint':
+            state = cmds.getAttr('%s.displayLocalAxis' % sel)
+            cmds.setAttr('%s.displayLocalAxis' % sel, not state)
+            cmds.setAttr('%s.jointOrientX' % sel, keyable=not state, channelBox=not state)
+            cmds.setAttr('%s.jointOrientY' % sel, keyable=not state, channelBox=not state)
+            cmds.setAttr('%s.jointOrientZ' % sel, keyable=not state, channelBox=not state)
+            
+display_joint_orient()
